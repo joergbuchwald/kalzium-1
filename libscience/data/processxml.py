08f29801ecf4981c83b7f256552d35dd4377116c
joergbuchwald <joerg.buchwald@ufz.de>
2019-09-26 18:07:34 +0200

    updating process.xml

--- libscience/data/processxml.py
+++ libscience/data/processxml.py
@@ -10,10 +10,27 @@ for child in root.getchildren():
     elements.append(child.attrib.values()[0])
     isotopes=[]
     numbers=[]
+    masses=[]
+    halflifes=[]
+    atomicnumbers=[]
     for grandchild in child.getchildren():
         isotopes.append(grandchild.attrib.values()[0])
         numbers.append(grandchild.attrib.values()[1])
-    d={'isotop': isotopes, 'number': numbers}
+        mass=""
+        halflife=""
+        atomicnumber=""
+        for grandgrandchild in grandchild.getchildren():
+            if grandgrandchild.attrib.values()[0]=="bo:exactMass":
+                mass=grandgrandchild.text
+            if grandgrandchild.attrib.values()[0]=="bo:halfLife":
+                halflife=grandgrandchild.text
+            if grandgrandchild.attrib.values()[0]=="bo:atomicNumber":
+                atomicnumber=grandgrandchild.text
+        masses.append(mass)
+        halflifes.append(halflife)
+        atomicnumbers.append(atomicnumber)
+
+    d={'isotop': isotopes, 'number': numbers, 'mass': masses, 'halflife': halflifes, 'atomicnumber': atomicnumbers}
     dataframes.append(pd.DataFrame(data=d))
 for i,element in enumerate(elements):
     print(dataframes[i])
