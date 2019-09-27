b6a1ab12b1b8fa8e7b8572476b663541b86f9b53
joergbuchwald <joerg.buchwald@ufz.de>
2019-09-27 19:43:35 +0200

    updating isotope scripts

--- libscience/data/processxml.py
+++ libscience/data/processxml.py
@@ -13,12 +13,18 @@ for child in root.getchildren():
     masses=[]
     halflifes=[]
     atomicnumbers=[]
+    relativeabundances=[]
+    spins=[]
+    magneticmoments=[]
     for grandchild in child.getchildren():
         isotopes.append(grandchild.attrib.values()[0])
         numbers.append(grandchild.attrib.values()[1])
         mass=""
         halflife=""
         atomicnumber=""
+        relativeabundance=""
+        spin=""
+        magneticmoment=""
         for grandgrandchild in grandchild.getchildren():
             if grandgrandchild.attrib.values()[0]=="bo:exactMass":
                 mass=grandgrandchild.text
@@ -26,11 +32,20 @@ for child in root.getchildren():
                 halflife=grandgrandchild.text
             if grandgrandchild.attrib.values()[0]=="bo:atomicNumber":
                 atomicnumber=grandgrandchild.text
+            if grandgrandchild.attrib.values()[0]=="bo:relativeAbundance":
+                relativeabundance=grandgrandchild.text
+            if grandgrandchild.attrib.values()[0]=="bo:spin":
+                spin=grandgrandchild.text
+            if grandgrandchild.attrib.values()[0]=="bo:magneticMoment":
+                magneticmoment=grandgrandchild.text
         masses.append(mass)
         halflifes.append(halflife)
         atomicnumbers.append(atomicnumber)
+        relativeabundances.append(relativeabundance)
+        spins.append(spin)
+        magneticmoments.append(magneticmoment)
 
-    d={'isotop': isotopes, 'number': numbers, 'mass': masses, 'halflife': halflifes, 'atomicnumber': atomicnumbers}
+    d={'isotop': isotopes, 'number': numbers, 'mass': masses, 'spin': spins, 'magnetic_moment': magneticmoments, 'relative_abundance': relativeabundances, 'halflife': halflifes, 'atomicnumber': atomicnumbers}
     dataframes.append(pd.DataFrame(data=d))
 for i,element in enumerate(elements):
     print(dataframes[i])
